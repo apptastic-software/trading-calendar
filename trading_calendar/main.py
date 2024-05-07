56435b4b3e2f44747c09bebe6b839fdaf3efeb80
Peter Westling <w3stling@users.noreply.github.com>
2024-05-07 19:48:35 +0200

    Bump version to 0.0.20

--- trading_calendar/main.py
+++ trading_calendar/main.py
@@ -15,7 +15,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 
 def version():
-    return "0.0.19"
+    return "0.0.20"
 
 print("Trading Calendar version: {}".format(version()))
 
